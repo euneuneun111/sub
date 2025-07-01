c0b49b5d9baa60b165175e654a8af38855e65c5d
user <user@DESKTOP-AS8H76J>
2025-07-01 16:37:19 +0900

    0701

---
+++ spring_DI_IoC/src/test/java/spring_DI_IoC/TestBasicDataSource.java
@@ -0,0 +1,11 @@
+package spring_DI_IoC;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:com/spring/context/dataSource-context.xml")
+public class TestBasicDataSource {
+	
+}
